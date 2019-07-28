34e72a867df3afe1614470e3a591dd52860df895
Jordan McRae <imjordanm@gmail.com>
2019-07-28 20:08:03 +1200

    Attempt to fix env variables for production.

--- theme/sanity/dashboardConfig.js
+++ theme/sanity/dashboardConfig.js
@@ -15,7 +15,7 @@ export default {
             title: "SeniorNet Website",
             apiId: "058e206e-a0b6-477b-ae36-4c7637c0c443",
             buildHookId:
-              "https://api.netlify.com/build_hooks/5d3a89cb07c2becc2030c35f",
+              "https://api.netlify.com/build_hooks/5d3d55a75da00007a92aa3e8",
             name: "naughty-chandrasekhar-75b3be",
           },
         ],
